4a98fb0d5e89fcf6b4114e6bc7d374fefd7c7367
jim-low <robil12380@gmail.com>
2023-10-31 06:31:44 +0800

    chore: cleanup and add prettier configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParents: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth: 2,
+    trailingComa: 'none'
+}
+
+module.exports = options;
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import logo from './logo.svg'
-import "./App.css";
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-        <header className="App-header">
-        <img className='App-logo' src={logo} alt="its the fucking React Logo"/>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" src={logo} alt="its the fucking React Logo" />
         <p>
-        Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-        Learn React
+          Learn React
         </a>
-        </header>
-        </div>
-    );
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
